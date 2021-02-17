44b44b7b11a03c82af60a7ecd2d5d97b1c620ac0
greedy0111 <greedy0111@naver.com>
2021-02-17 23:40:24 +0900

    21. useReducer - App에서 useReducer 사용하기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useMemo, useCallback } from 'react';
+import React, { useRef, useReducer, useMemo, useCallback } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 
@@ -6,21 +6,12 @@ function countActiveUsers(users) {
   console.log('활성된 사용자 수를 세는중...')
   return users.filter(user => user.active).length; // true 값만 가져옴
 }
-function App() {
-  const [inputs, setInputs] = useState({
+const initialState = {
+  inputs: {
     username: '',
     email: ''
-  });
-  const { username, email } = inputs; // 현재값 밖으로 빼줌
-  const onChange = useCallback (e => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value
-    });
-  }, [inputs]);
-
-  const [users, setUsers] = useState([ // 컴포넌트의 상태로 관리
+  },
+  users: [
     {
       id: 1,
       username: 'velopert',
@@ -39,52 +30,97 @@ function App() {
       email: 'liz@example.com',
       active: false,
     }
-  ]);
+  ]
+}
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CHANGE_INPUT':
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.name]: action.value
+        }
+      };
+    case 'CREATE_USER':
+      return {
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user)
+      }
+    case 'TOGGLE_USER':
+      return {
+        ...state,
+        users: state.users.map(user => 
+          user.id === action.id ? {...user, active: !user.active} : user 
+        )
+      }
+    case 'REMOVE_USER':
+      return {
+        ...state,
+        users: state.users.filter(user => user.id !== action.id)
+      }
+    default:
+      throw new Error('Unhandled action');
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   const nextId = useRef(4);
-  const onCreate = useCallback(() => {
-    const user = {
-      id: nextId.current,
-      username,
-      email
-    };
-    // setUsers(users.concat(user));
-    setUsers(users => [...users, user]); // 기존 배열 복사, 새로운 배열 등록 
+  const {users} = state;
+  const {username, email} = state.inputs;
 
-    setInputs({ // 초기화 버튼 클릭시 초기화
-      username: '',
-      email: ''
+  const onChange = useCallback(e => {
+    const {name, value} =e.target;
+    dispatch({
+      type:'CHANGE_INPUT',
+      name,
+      value
+    })
+  }, []);
+
+  const onCreate = useCallback(() => {
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id: nextId.current,
+        username,
+        email,
+      }
     });
     nextId.current += 1;
-  }, [username, email]);
-
-  const onRemove = useCallback(id => {
-    setUsers(users => users.filter(user => user.id !== id)); // 불변성 필요
-  }, []);
+  }, [username, email])
 
   const onToggle = useCallback(id => {
-    setUsers(users => users.map( // 불변성 필요
-      user => user.id === id ? {...user, active: !user.active} : user // ... 불변성 필요
-    )) 
+    dispatch({
+      type: 'TOGGLE_USER',
+      id
+    });
   }, []);
-  // 값을 등록할 때 : onCreate
-  // spread를 사용하거나, concat 함수 사용.
-  
-  // 값을 제거 할 때 : onRemove
-  // filter 함수 사용.
-  
-  // 특정 값만 업데이트 해줄 때 : onToggle
-  // map 함수 사용.
-  const count = useMemo(() => countActiveUsers(users), [users]); // [users]값이 바뀌어야만 새로 연산해주겠다. 그렇지 않으면 재사용 
+
+  const onRemove = useCallback(id => {
+    dispatch({
+      type: "REMOVE_USER",
+      id
+    })
+  }, [])
+
+  const count = useMemo(() => countActiveUsers(users), [users])
+
   return (
     <>
-      <CreateUser
-        username={username}
-        email={email}
+      <CreateUser 
+        username={username} 
+        email={email} 
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <UserList 
+        users={users} 
+        onToggle={onToggle} 
+        onRemove={onRemove}
+      />
       <div>활성 사용자 수 : {count}</div>
     </>
   );
